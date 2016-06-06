993ac41308cd05c58a1ef47de0a0cad611fe836a
Chris Caragianis <chriscaragianis@gmail.com>
2016-06-06 11:17:22 -0400

    Aggregate#attach attaches data in a key

--- spec/aggregateObjectSpec.js
+++ spec/aggregateObjectSpec.js
@@ -9,4 +9,13 @@ describe('Aggregate', () => {
       expect(new Aggregate({name: "Travis"}).name).toEqual("Travis");
     });
   });
+
+  describe('attach', () => {
+
+    it('attaches an object', () => {
+      var subject = new Aggregate();
+      subject.attach('newData', {eyes: 'brown'});
+      expect(subject.newData.eyes).toEqual('brown');
+    });
+  });
 });
