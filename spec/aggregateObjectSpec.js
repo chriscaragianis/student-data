4492ce77213b5f6587328057f346a1b366042c6c
Chris Caragianis <chriscaragianis@gmail.com>
2016-06-06 11:27:24 -0400

    Aggregate#detach takes the data off

--- spec/aggregateObjectSpec.js
+++ spec/aggregateObjectSpec.js
@@ -17,5 +17,22 @@ describe('Aggregate', () => {
       subject.attach('newData', {eyes: 'brown'});
       expect(subject.newData.eyes).toEqual('brown');
     });
+
+    it('does not duplicate', () => {
+      var subject = new Aggregate();
+      subject.attach('newData', {eyes: 'brown'});
+      subject.attach('newData', {eyes: 'blue'});
+      expect(subject.newData.eyes).toEqual('brown');
+    });
+  });
+
+  describe('detach', () => {
+
+    it('removes data from the key', () => {
+      var subject = new Aggregate();
+      subject.attach('newData', {eyes: 'brown'});
+      subject.detach('newData');
+      expect(subject.newData).not.toBeDefined();
+    });
   });
 });
